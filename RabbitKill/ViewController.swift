af37f501cd4815718230a83b2080743599d244ae
Marco <bernardimarco52@gmail.co>
2019-04-04 11:58:44 +0200

    First Test Version

--- RabbitKill/ViewController.swift
+++ RabbitKill/ViewController.swift
@@ -10,9 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var score: UILabel!
+    var click = 0
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
+        var timer:Timer?
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(onTimerFires), userInfo: nil, repeats: true)
+
+
+    }
+    func RandomButton(){
+        let button = UIButton(type: UIButton.ButtonType.system) as UIButton
+        let x = UIScreen.main.bounds.width
+        let y = UIScreen.main.bounds.height
+        let buttonWidth:CGFloat = 150
+        let buttonHeight:CGFloat = 45
+        let xPostion:CGFloat = CGFloat.random(in: 0 ..< x - buttonWidth)
+        let yPostion:CGFloat = CGFloat.random(in: 0 ..< y - buttonHeight)
+        button.frame = CGRect(x:xPostion, y:yPostion, width:buttonWidth, height:buttonHeight)
+        
+        button.backgroundColor = UIColor.lightGray
+        button.setTitle("Tap me", for: UIControl.State.normal)
+        button.tintColor = UIColor.black
+        button.addTarget(self, action: #selector(ViewController.buttonAction(_:)), for: .touchUpInside)
+        
+        self.view.addSubview(button)
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
+            button.removeFromSuperview()
+        }
+        
+    }
+    @objc func onTimerFires()
+    {
+        
+        RandomButton()
+    }
+    @objc func buttonAction(_ sender:UIButton!)
+    {
+        click += 1
+        score.text = String(click)
+        sender.removeFromSuperview()
+        print("Button tapped")
     }
 
 
